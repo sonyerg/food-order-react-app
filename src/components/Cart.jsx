1d5bff9cd78d174cff4468691ad255c5f232730a
sonyerg <ericksonrguinto@gmail.com>
2024-04-08 22:22:48 +1200

    added cart modal and its functions

---
+++ src/components/Cart.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting.js";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+export default function Cart() {
+  const { items } = useContext(CartContext);
+  const { progress, hideCart } = useContext(UserProgressContext);
+
+  const cartTotal = items.reduce((totalPrice, item) => {
+    return totalPrice + item.quantity * item.price;
+  }, 0);
+
+  function handleCloseCart() {
+    hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
